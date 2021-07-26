188958315cee0014ba0ae4dbef6b252325df2020
Nayem Khan <nayemk900@gmail.com>
2021-07-26 22:36:30 +0600

    properties added

--- src/components/Home/Properties/PropertyCard.js
+++ src/components/Home/Properties/PropertyCard.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PropertyCard = ({ img, title, index }) => {
     return (
+
         <div className={`card-area card-${index}`}>
-            <img src={img} alt={title} />
-            <div className="content">
-                <h4 className="title">{title}</h4>
-            </div>
+            <Link to='/detail'>
+                <img src={img} alt={title} />
+                <div className="content">
+                    <h4 className="title">{title}</h4>
+                </div>
+            </Link>
+
         </div>
+
+
     );
 };
 
