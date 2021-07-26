6bb4a9c27e00455df8aada6910adc0588d02965e
Habibtusher <tusherhrt@gmail.com>
2021-07-26 21:33:01 +0600

    save

--- src/components/PropertyDetailPage/PropertyDetailPage/PropertyDetailPage.js
+++ src/components/PropertyDetailPage/PropertyDetailPage/PropertyDetailPage.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import "./PropertyDetailPage.css"
+import Header from '../../Home/Header/Header/Header';
 import PropertyDetail from '../PropertyDetail/PropertyDetail';
 
 const PropertyDetailPage = () => {
     return (
-        <div className="container row mx-auto">
+        <div>
+            <Header/>
+            <div className="container row mx-auto propertyDetailPage">
 
-            <div className="col-md-7">
-                <PropertyDetail />
-            </div>
-            <div className=" col-md-4">
-            <p className="ml-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo totam similique. Sed, aut! Voluptate, mollitia porro dolorum voluptatum culpa omnis itaque corrupti veniam aliquam sunt voluptatem atque ullam magni.
-            </p>
-            </div>
+                <div className="col-md-7">
+                    <PropertyDetail />
+                </div>
+                <div className=" col-md-4">
+                    <p className="ml-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo totam similique. Sed, aut! Voluptate, mollitia porro dolorum voluptatum culpa omnis itaque corrupti veniam aliquam sunt voluptatem atque ullam magni.
+                    </p>
+                </div>
 
+            </div>
         </div>
     );
 };
