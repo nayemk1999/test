7e499db00b96ce35b5d45b7c6e3038c2ab805323
Nayem Khan <nayemk900@gmail.com>
2021-07-27 19:38:22 +0600

    search filter location

--- src/components/Home/SearchBar/SearchBar.js
+++ src/components/Home/SearchBar/SearchBar.js
@@ -18,7 +18,7 @@ export default function SearchBar() {
 
     const divisionChange = (e) => {
         const select = allDivisionbn.find(id => e.target.value === id.divisionbn)
-        setSelectId(select._id) 
+        setSelectId(select._id)
     }
 
     useEffect(() => {
@@ -28,7 +28,8 @@ export default function SearchBar() {
     }, [selectId])
 
     const districtChange = (e) => {
-        console.log(e.target);
+        const select = district.find(id => e.target.value === id.district)
+        setupazila(select.upazilla)
     }
     return (
         <div class="search-container " aria-label="Mini form">
@@ -37,18 +38,21 @@ export default function SearchBar() {
                     <select {...register("বিভাগ")} onChange={divisionChange} className="form-select me-2" aria-label="Default select example">
                         <option selected>বিভাগ</option>
                         {
-                            allDivisionbn.map(dv => <option id={dv._id}>{dv.divisionbn}</option>)
+                            allDivisionbn.map(dv => <option key={dv._id}>{dv.divisionbn}</option>)
                         }
 
                     </select>
-                    <select {...register("জেলা")} className="form-select me-2" aria-label="Default select example">
+                    <select {...register("জেলা")} onChange={districtChange} className="form-select me-2" aria-label="Default select example">
                         <option selected>জেলা</option>
-                        { selectId &&
-                            district.map(dis => <option id={dis._id}>{dis.district}</option>)
+                        {selectId &&
+                            district?.map(dis => <option key={dis._id}>{dis.district}</option>)
                         }
                     </select>
                     <select {...register("উপজিলা")} className="form-select me-2" aria-label="Default select example">
                         <option selected>উপজিলা</option>
+                        {selectId &&
+                            upazila.map(upa => <option key={upa}>{upa}</option>)
+                        }
                     </select>
                     <button class="btn btn-outline-success" type="submit">Find</button>
                 </div>
@@ -61,15 +65,18 @@ export default function SearchBar() {
                     </select>
                     <select {...register("price")} class="form-select me-2" aria-label="Default select example">
                         <option selected>Price</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="1">12,002,000</option>
+                        <option value="2">12,002,000</option>
+                        <option value="3">12,002,000</option>
                     </select>
                     <select {...register("beds")} class="form-select me-2" aria-label="Default select example">
                         <option selected>Beds</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="3">4</option>
+                        <option value="3">5</option>
+                        <option value="3">6</option>
                     </select>
                 </div>
             </form>
