c8e9bdde138c18b2734240b33255f455944300e2
Nayem <nayemk900@gmail.com>
2021-08-03 17:41:54 +0600

    firebase deploy

--- src/firebaseConfig/firebaseConfig.js
+++ src/firebaseConfig/firebaseConfig.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyATZGHI6dDo3-ARLqKia6M7WlG7nlhxvKY",
-    authDomain: "toprak-real-estate-ad43f.firebaseapp.com",
-    projectId: "toprak-real-estate",
-    storageBucket: "toprak-real-estate.appspot.com",
-    messagingSenderId: "989676931099",
-    appId: "1:989676931099:web:a70529f5308ab2b15c50be"
+  export const firebaseConfig = {
+    apiKey: "AIzaSyDwqOP1xWWHF1SN7W3VbcsAE-YWIXWAmIA",
+    authDomain: "toprak-real.firebaseapp.com",
+    projectId: "toprak-real",
+    storageBucket: "toprak-real.appspot.com",
+    messagingSenderId: "1061136894985",
+    appId: "1:1061136894985:web:bdf07f57db287b43ca7cd8"
   };
