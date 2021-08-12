d97937761ec71eec5d789184231e6ea0846e1012
Nayem Khan <nayemk900@gmail.com>
2021-08-12 20:03:06 +0600

    test

--- src/components/Dashboard/ManagePost/ManagePost.js
+++ src/components/Dashboard/ManagePost/ManagePost.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-
-
-
 const ManagePost = () => {
  
 
-
     return (
         <div>
-<<<<<<< HEAD
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form>
                 <div className="from-group mb-3">
                     <label htmlFor="email">Enter Email :</label>
-                    <input className="form-control" name='email' {...register("email")} />
+                    {/* <input className="form-control" name='email' {...register("email")} /> */}
                 </div>
 
                 <button type="submit " className="btn btn-primary">Add Admin</button>
             </form>
-=======
-            <h3>Manage post</h3>
->>>>>>> 88ca1aa2739704c1081877e40e8706f1a1383021
         </div>
     );
 };
