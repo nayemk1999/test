6f74a03fb0c69b56997626d5f1b76f28ef99a852
Mahmudulk675 <mahmudulk675@gmail.com>
2021-08-12 20:02:16 +0600

    ui design

--- src/components/Home/UserReview/UserReview.js
+++ src/components/Home/UserReview/UserReview.js
@@ -1,24 +1,25 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 
-
-import Review from './Review';
+import Review from "./Review";
 
 export default function UserReview() {
-const [review,setReview] = useState([])
+  const [review, setReview] = useState([]);
 
   useEffect(() => {
-    fetch('https://toprakserver.herokuapp.com/review/all-review')
-    .then(res=>res.json())
-    .then(data=> setReview(data))
-  }, [])
+    fetch("https://toprakserver.herokuapp.com/review/all-review")
+      .then((res) => res.json())
+      .then((data) => setReview(data));
+  }, []);
   return (
     <div className="mt-5">
-      <h2 className='text-center mt-5 h1Style'>Client  <span style={{ color: "#61dfed" }}>Feedback</span></h2>
+      <h2 className="text-center mt-5 h1Style">
+        Client <span style={{ color: "#61dfed" }}>Feedback</span>
+      </h2>
 
       <div className="row mt-5" style={{ width: "80%", margin: "0 auto" }}>
-        {
-          review.map(review => <Review key={review._id} review={review}></Review>)
-        }
+        {review.map((review) => (
+          <Review key={review._id} review={review}></Review>
+        ))}
       </div>
     </div>
   );
