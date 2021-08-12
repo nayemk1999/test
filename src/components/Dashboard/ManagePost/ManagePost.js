0a007db9a2f2c46262eaf11da41c92c3e1506f72
Tanvir Hossain Fahim <tanvirhossainfahim89@gmail.com>
2021-08-12 16:17:31 +0600

    managepost update need

--- src/components/Dashboard/ManagePost/ManagePost.js
+++ src/components/Dashboard/ManagePost/ManagePost.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-
-
-
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const ManagePost = () => {
- 
-
-
-    return (
-        <div>
-            <h3>Manage post</h3>
-        </div>
-    );
+  //   const { managePost, SetManagePost } = useEffect();
+  return (
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Index Num.</th>
+          <th scope="col">Property Name</th>
+          <th scope="col">Owner Name</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Black House</td>
+          <td>Fahim</td>
+          <td>Edit/ Delete</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Marcidies</td>
+          <td>Habibur</td>
+          <td>Edit/ Delete</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Angels House</td>
+          <td>Nayem</td>
+          <td>Edit/ Delete</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default ManagePost;
