cd75256d4043aa35fcab5267beed741c6416dab9
Nayem Khan <nayemk900@gmail.com>
2021-08-21 20:30:45 +0600

    profile style

--- src/components/Dashboard/Profile/Profile.js
+++ src/components/Dashboard/Profile/Profile.js
@@ -8,6 +8,7 @@ import { UserContext } from "../../../App";
 const Profile = () => {
   const { loggedInUser, setLoggedInUser } = useContext(UserContext);
   const history = useHistory();
+
   const signOut = () => {
     // initializeLoginFramework();
     const userId = localStorage.removeItem("user");
@@ -20,50 +21,43 @@ const Profile = () => {
         });
     }
   };
-  return (
-    <Container
-      style={{
-        maxWidth: "30rem",
-        position: "relative",
-        zIndex: -1,
-      }}
-    >
-      <Card className="border-0 shadow">
-        <Card.Header as={"h3"} className="text-center border-0 mt-3 pt-2 pb-2">
-          Profile
-        </Card.Header>
-        <Card.Body className="card-body">
-          <div className="d-flex flex-column align-items-center text-center">
-            <img
-              src={
-                loggedInUser.profilePicture ||
-                "https://i.ibb.co/7CzR0Dg/users.jpg"
-              }
-              alt="..."
-              className="rounded-circle"
-              width="150"
-              style={{ boxShadow: "0 8px 20px -4px #95abbb" }}
-            />
-            <div className="mt-4">
-              <h4>{loggedInUser.username}</h4>
-              <p className="text-secondary mb-3">{loggedInUser.email}</p>
-            </div>
-            <div className="mt-2">
-              <Button
-                as={Link}
-                to="/dashboard/update-profile"
-                className="btn btn-secondary me-4"
-              >
-                Profile Update
-              </Button>
-              <Button onClick={signOut} className="btn btn-secondary">
-                Log Out
-              </Button>
-            </div>
+  return (<Container>
+    <Card className="border-0 shadow">
+      <Card.Header as={"h3"} className="text-center border-0 mt-3 pt-2 pb-2">
+        Profile
+      </Card.Header>
+      <Card.Body className="card-body">
+        <div className="d-flex flex-column align-items-center text-center">
+          <img
+            src={
+              loggedInUser.profilePicture ||
+              "https://i.ibb.co/7CzR0Dg/users.jpg"
+            }
+            alt="..."
+            className="rounded-circle"
+            width="150"
+            style={{ boxShadow: "0 8px 20px -4px #95abbb" }}
+          />
+          <div className="mt-4">
+            <h4>{loggedInUser.username}</h4>
+            <p className="text-secondary mb-3">{loggedInUser.email}</p>
           </div>
-        </Card.Body>
-      </Card>
-    </Container>
+          <div className="mt-2">
+            <Button
+              as={Link}
+              to="/dashboard/update-profile"
+              className="btn btn-secondary me-4"
+            >
+              Profile Update
+            </Button>
+            <Button onClick={signOut} className="btn btn-secondary">
+              Log Out
+            </Button>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  </Container>
   );
 };
 
